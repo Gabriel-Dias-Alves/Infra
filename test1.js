239be95edf9590d425cd3007f2a41160bba09fb5
Gabriel-Dias-Alves <gabrielalvesrad@gmail.com>
2026-08-24 20:18:05 -0300

    Aula js 24/08

---
+++ test1.js
@@ -0,0 +1,42 @@
+ nome="Gabriel Alves ";
+        idade="28 anos";
+        cidade="Recife";
+        preço="1,99"
+        biscoito="Futurinhos Black"
+
+        console.log("Meu nome é " + nome + ", tenho " + idade + ", moro em " + cidade + " e o preço do meu biscoito favorito é R$" + preço + " - " + biscoito);
+        console.log(preço==idade)
+        
+//Declarando variáveis
+valor1 = 10;
+valor2 = 20;
+valor3 = 30;
+valor4 = 40;
+
+//IF1
+if (valor1 < valor2) {
+    console.log("IF 1: valor1 é menor que valor2");
+}
+//IF2
+if (valor3 > valor4) {
+    console.log("IF 2: valor3 é maior que valor4");
+} else {
+    console.log("IF 2: valor3 não é maior que valor4");
+}
+
+//Comparação dos 4 valores
+console.log("Comparação dos 4 valores:");
+
+console.log("valor1 == valor2:", valor1 == valor2);
+console.log("valor2 < valor3:", valor2 < valor3);
+console.log("valor3 < valor4:", valor3 < valor4);
+console.log("valor4 > valor1:", valor4 > valor1);
+
+//Quatro operações matemáticas
+console.log("Operações matemáticas:");
+
+console.log("Adição:", valor1 + valor2);
+console.log("Subtração:", valor4 - valor3);
+console.log("Multiplicação:", valor2 * valor3);
+console.log("Divisão:", valor4 / valor1);
+        
