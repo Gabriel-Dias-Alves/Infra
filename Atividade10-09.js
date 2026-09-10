40b6636be084f3f34db2a6e823c7a96cb9c39e61
Gabriel-Dias-Alves <gabrielalvesrad@gmail.com>
2026-09-10 20:51:39 -0300

    ATIVIDADE CONCLUIDA

---
+++ Atividade10-09.js
@@ -0,0 +1,17 @@
+//Exercício: O Verificador de Beco e Cinema
+
+var ingreço = true
+var idade = 18
+
+if (ingreço == true && idade >= 18){
+    console.log("Pode entrar, Tenha um bom filme!")
+}
+
+else if (ingreço == false || idade < 18) {
+    console.log("Opa! Esse filme não é para você, vai assistir pepa pig!")
+}
+
+else{(ingreço == false || idade >= 60)
+    console.log("FIlmaço em! Soube que ele morre no final!")
+    
+}
---
+++ if.js
@@ -0,0 +1,19 @@
+// introdução ao if
+
+//if (condição) {operadores logicos
+  //  instrução
+//}
+
+var dinheiro = true
+var comida = "acaraje"
+if ( dinheiro == true && comida == "acaraje"){
+    console.log("Eba! chegou o seu acarajezinhoooooo!")
+    }
+
+    else if (dinheiro == false || comida != "acaraje") {
+        console.log ("dançou, ficou na vontade")
+    }
+
+    else{
+        console.log("RIP, vai na paz!")
+    }
